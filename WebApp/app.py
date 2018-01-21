838f1baee877c50919294f76c8ed205a10afa2e2
gaurav98m <gauravm@berkeley.edu>
2018-01-21 01:30:57 -0800

    WebApp

---
+++ WebApp/app.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from flask import Flask
+from models import *
+app = Flask(__name__)
+@app.route('/')
+def index():
+  school_count = School.select().count()
+  schools = School.select().order_by(School.school_name.asc())
+  return render_template('index.html', count=school_count, schools=schools)
+@app.route('/schools/clemente')
+def school():
+    return render_template('school.html')
+if __name__ == '__main__':
+    app.run(debug=True)
