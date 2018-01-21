1b5746dbbdfa2482172cb048afb36fdc7939c2a4
bikramjits <bikramjit@berkeley.edu>
2018-01-20 20:54:10 -0800

    Update to app.py Still trial phase.

---
+++ app.py
@@ -0,0 +1,36 @@
+"""
+Made for Hack Davis 2018 
+
+Building Occupancy Finder for UC Davis 
+
+By 
+Bikramjit Singh Kukreja and Gaurav Mulchandani
+"""
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+#Take in Electricity Data from known buildings on Campus. 
+user = "ou\piapihack2018"
+passw =  "Go $ave energy, 2018!"
+url = "https://ucd-piwebapi.ou.ad3.ucdavis.edu/piwebapi/attributes/search?databasewebid=F1RDbgZy4oKQ9kiBiZJTW7eugwJztPDnRh5UOIr8WoWgnq3gVVRJTC1BRlxDRUZT&query=%20Element:{Name:=Electricity}%20Name:=Demand"  
+
+req = requests.post(url, data = {}, auth=HTTPBasicAuth(user, passw))
+
+json_data = json.load(req)
+
+new_var = json_data["GetAttributes"]["Headers"]["Content"]["Items"][0]
+
+
+
+
+
+
+#Convert json into python objects (Probably for loop for multiple buildings considering
+# the length of the request time). 
+
+
+
+
+#Logic for calculating Occupancy and ranking systems. 
+
