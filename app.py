0006df2b87e227eb374446ba94eaef214dd20e7c
gaurav98m <gauravm@berkeley.edu>
2018-01-21 06:26:52 -0800

    Database

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 """
-Made for Hack Davis 2018 
+Made for Hack Davis 2018
 
-Building Occupancy Finder for UC Davis 
+Building Occupancy Finder for UC Davis
 
-By 
+By
 Bikramjit Singh Kukreja and Gaurav Mulchandani
 """
 import json
@@ -11,8 +11,9 @@ import numpy as np
 import requests
 import pprint
 from requests.auth import HTTPBasicAuth
+from datascience import *
 
-#Take in Electricity Data from known buildings on Campus. 
+#Take in Electricity Data from known buildings on Campus.
 
 user = "ou\piapihack2018"
 passw =  "Go $ave energy, 2018!"
@@ -41,15 +42,22 @@ json_data = json.loads(req.text)
 new_var = json_data["values"]["Content"]["Items"]
 
 new_arr = []
-for i in range(100): 
+for i in range(100):
 	new_arr.append(new_var[i]["Content"]["Value"])
 
-
 #Convert json into python objects (Probably for loop for multiple buildings considering
-# the length of the request time). 
+# the length of the request time).
 
 
 
-
-#Logic for calculating Occupancy and ranking systems. 
-
+#Logic for calculating Occupancy and ranking systems.
+Area= Table.read_table('Area_2.csv')
+Baseline= Table.read_table('baseline.csv')
+Result= Baseline.join('Name', Area)
+Buildings = Table.read_table('buildings.csv').with_column('Current', new_arr)
+Final= Buildings.join('Name', Result)
+Final= Final.with_column('FinalR', (Final.column(1)-Final.column(2))/Final.column(3))
+Final=Final.sort(4)
+Final.to_csv('my_table.csv')
+df = pandas.read_csv('my_table.csv')
+Ranking= df.to_sql(table_name, conn, if_exists='append', index=False)
