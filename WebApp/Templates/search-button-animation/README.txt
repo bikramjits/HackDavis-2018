cd6b99baaff602fbcb3b6b50236a23d5ae3d07db
gaurav98m <gauravm@berkeley.edu>
2018-01-21 04:45:15 -0800

    WebApp3

---
+++ WebApp/Templates/search-button-animation/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/kristyjy/pen/zGOXYb.
+
+ Based on Dribbble: https://dribbble.com/shots/2026784-Search-Animation
--- WebApp/app.py
+++ WebApp/app.py
@@ -4,9 +4,9 @@ from models import *
 app = Flask(__name__)
 @app.route('/')
 def index():
-  school_count = School.select().count()
-  schools = School.select().order_by(School.school_name.asc())
-  return render_template('index.html', count=school_count, schools=schools)
+  # school_count = School.select().count()
+  # schools = School.select().order_by(School.school_name.asc())
+  return render_template('index.html')
 @app.route('/schools/clemente')
 def school():
     return render_template('school.html')
